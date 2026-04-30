1bc38a119b186208784099a156aa122706ed6e49
NathanThus <nathan.thus@outlook.com>
2026-04-30 20:42:20 +0200

    ✨ Update PlayerRotation to allow offsets

--- PlayerRotation.cs
+++ PlayerRotation.cs
@@ -4,11 +4,24 @@ using System;
 public partial class PlayerRotation : Node3D
 {
 	[Export(PropertyHint.Range, "-1f,1f,")]
-	public float RotationSpeed {get; set;} = 0.05f;
-
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public float RotationSpeed { get; set; } = 0.05f;
+	[Export]
+	public Node3D RotationTarget { get; set; }
+	[Export]
+	public Node3D OffsetTarget { get; set; }
+	[Export]
+	public float RotationDistance {get; set;} = 10f;
+	[Export]
+	public float RotationHeight {get; set;} = 10f;
+	
 	public override void _Process(double delta)
 	{
-		this.Rotate(Vector3.Up, RotationSpeed * (float)delta);
+		UpdateDistance();
+		RotationTarget.Rotate(Vector3.Up, RotationSpeed * (float)delta);
+	}
+
+	public void UpdateDistance()
+	{
+		OffsetTarget.Position = new Vector3(RotationTarget.Position.X, RotationHeight, RotationDistance);
 	}
 }
