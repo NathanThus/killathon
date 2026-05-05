3e12512e83cf79a04d33ad1147558c0139dd6096
NathanThus <nathan.thus@outlook.com>
2026-05-06 01:26:33 +0200

    💥 Complete overhaul of spawning & collision
    TLDR: It works, but the signal system gives me headaches. I understand the design philosophy, but realistically the callable methods SHOULD be added to C# as well. The fact it isn't and relies on the developer checking the exact wording and arguments is beyond ridiculous. Either way, lessons learned the hard way are rarely forgotten, so I doubt I'll repeat this fun little sidequest.

---
+++ Scripts/Enemy/EnemyController.cs
@@ -0,0 +1,49 @@
+using System;
+using Godot;
+
+public partial class EnemyController : CharacterBody3D
+{
+	[Export]
+	public NavigationAgent3D NavigationAgent { get; set; }
+	[Export]
+	public EnemyHealth Health { get; set; }
+	[Export]
+	public CharacterBody3D Body { get; set; }
+	[Export]
+	public float MovementSpeed { get; set; } = 2.0f;
+
+	public Vector3 MovementTarget
+	{
+		get { return NavigationAgent.TargetPosition; }
+		set { NavigationAgent.TargetPosition = value; }
+	}
+
+	public override void _Ready()
+	{
+		Health.OnDeath += HandleDeath;
+		base._Ready();
+	}
+
+
+	public override void _PhysicsProcess(double delta)
+	{
+		if (NavigationAgent.IsNavigationFinished())
+		{
+			return;
+		}
+
+		Vector3 currentAgentPosition = Body.GlobalTransform.Origin;
+		Vector3 nextPathPosition = NavigationAgent.GetNextPathPosition();
+
+		this.Velocity = currentAgentPosition.DirectionTo(nextPathPosition) * MovementSpeed;
+		Body.MoveAndSlide();
+		Body.LookAt(Vector3.Zero);
+	}
+
+	public void HandleDeath(EnemyHealth health)
+	{
+		health.OnDeath -= HandleDeath;
+		QueueFree();
+	}
+
+}
