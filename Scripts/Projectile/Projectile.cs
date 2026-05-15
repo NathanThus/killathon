bdcd4fa9e1df43e86cd987809d431edfdd1a2a28
NathanThus <nathan.thus@outlook.com>
2026-05-15 23:37:24 +0200

    ✨ Finish projectiel area-of-effect

--- Scripts/Projectile/Projectile.cs
+++ Scripts/Projectile/Projectile.cs
@@ -6,26 +6,26 @@ public partial class Projectile : RigidBody3D
 	[Export]
 	public int Damage { get; set; } = 20;
 	[Export]
-	public ShapeCast3D ShapeCast {get; set;}
+	public ShapeCast3D ShapeCast { get; set; }
 
 	public void OnBodyEntered(Node3D node)
 	{
-		// For now, just direct fire.
-		if (node is EnemyHealth enemy)
+		ShapeCast.ForceShapecastUpdate();
+		
+		if (!ShapeCast.IsColliding())
 		{
-			ShapeCast.ForceShapecastUpdate();
-			if(!ShapeCast.IsColliding()) return;
-			for (int i = 0; i < ShapeCast.GetCollisionCount(); i++)
+			QueueFree();
+			return;
+		} 
+
+		for (int i = 0; i < ShapeCast.GetCollisionCount(); i++)
+		{
+			if (ShapeCast.GetCollider(i) is EnemyHealth enemyHealth)
 			{
-				if(ShapeCast.GetCollider(i) is EnemyHealth enemyHealth)
-				{
-					enemyHealth.Damage(Damage);
-				}
+				enemyHealth.Damage(Damage);
 			}
 		}
-
 		// Probably hit something else
-
 		// VFX
 		QueueFree();
 	}
